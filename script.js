18dd62c058ed05b752e2f1905d6c3ab1eb30e1c6
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-15 00:22:01 -0400

    added fahrenheit symbol, used math.round

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $("#searchBar").on("click", function () {
     var userInput = $("#userInput").val()
 
+    // created the function getweather to include the onclick cities and the user input cities together
     getWeather(userInput);
 })
 
@@ -15,8 +16,6 @@ function getWeather(cityName) {
     })
         .then(function (response) {
 
-            // Transfer content to HTML
-
             $(".date").text("" + response.list[0].dt_txt);
             $(".city").text("" + response.city.name);
             $(".feelsLike").text("Feels like: " + response.list[0].main.feels_like);
@@ -25,21 +24,36 @@ function getWeather(cityName) {
             // $("icon").text("" + response.weather.icon);
 
 
-            // Convert the temp to fahrenheit
-            var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
-            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+            // Converted the temp to fahrenheit
+            // used math.round to get rid of the decimals in the fahrenheit 
+            var tempF = Math.round((response.list[0].main.temp - 273.15) * 1.80 + 32);
+            $(".tempF").text("Temperature " + tempF + ("°"));
+
+            var feelsLikeF = Math.round((response.list[0].main.feels_like - 273.15) * 1.80 + 32);
+            $(".feelsLikeF").text("Feels like " + feelsLikeF + "°");
         });
 
 }
 
+// calling the getweather function for each onclick city 
 
 $("#chantilly").click(function () {
     getWeather("Chantilly");
-
 });
 
+$("#fairfax").click(function () {
+    getWeather("Fairfax");
+});
 
+$("#reston").click(function () {
+    getWeather("Reston");
+});
 
+$("#herndon").click(function () {
+    getWeather("Herndon");
+});
+
+// prevent default to stop the form from going to the next page
 $("form").submit(function (e) {
     e.preventDefault();
 });
