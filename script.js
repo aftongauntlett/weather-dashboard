92f306bc7e847edc48b41e2a6fb8f61ae7550081
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-14 23:57:19 -0400

    fixed temp, created function for getweather

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $("#searchBar").on("click", function () {
     var userInput = $("#userInput").val()
-    var APIKey = "2c10bee5201ad77237d10e6a96cc7389";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + APIKey;
 
+    getWeather(userInput);
+})
+
+function getWeather(cityName) {
+    var APIKey = "2c10bee5201ad77237d10e6a96cc7389";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
 
     $.ajax({
         url: queryURL,
@@ -11,27 +15,31 @@ $("#searchBar").on("click", function () {
     })
         .then(function (response) {
 
-
             // Transfer content to HTML
 
-            $(".city").text("City" + response.city.name);
-            // $(".firstDate").text("Date:" + response.date);
-            // $(".temp").text("Temp: " + response.temp);
-            // $(".humidity").text("Humidity: " + response.main.humidity);
+            $(".date").text("" + response.list[0].dt_txt);
+            $(".city").text("" + response.city.name);
+            $(".feelsLike").text("Feels like: " + response.list[0].main.feels_like);
+            $(".humidity").text("Humidity: " + response.list[0].main.humidity);
+            // $(".uv").text("UV Index: " + );
+            // $("icon").text("" + response.weather.icon);
 
-            // // Convert the temp to fahrenheit
-            // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-            // // add temp content to html
-            // $(".temp").text("Temperature (K) " + response.main.temp);
-            // $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
+            // Convert the temp to fahrenheit
+            var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
         });
 
-})
+}
+
+
+$("#chantilly").click(function () {
+    getWeather("Chantilly");
+
+});
+
+
 
 $("form").submit(function (e) {
     e.preventDefault();
 });
-
-
