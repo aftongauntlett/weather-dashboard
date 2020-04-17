b9829d009234c21707f64ad86423035c49ad7d70
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-16 22:25:15 -0400

    fixed columns

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $("#searchBar").on("click", function () {
 function getWeather(cityName) {
     var APIKey = "2c10bee5201ad77237d10e6a96cc7389";
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
-    // tried adding UV API, but could not get it working
+
     // var UV = "http://api.openweathermap.org/data/2.5/uvi/forecast?" + cityName + "appid=&lat=&lon=&cnt=" + APIKey;
 
     $.ajax({
@@ -36,12 +36,11 @@ function getWeather(cityName) {
 
             // use response.list to create a for loop for your 5 day forecast
             // combined city and date to have display side by side
-            $(".icon").html("<img src='http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-            // $(".icon").text(response.list[0].weather[0].icon);
             $(".cityDate").text(" " + response.city.name + " " + moment(response.list[0].dt_txt).format("(MMM Do, YYYY)"));
             $(".humidity").text("Humidity: " + response.list[0].main.humidity + " %");
             // $(".uv").text("UV Index: " + );
             $(".wind").text("Wind Speed: " + response.list[0].wind.speed + (" MPH"));
+
             // Converted the temp to fahrenheit
             // used math.round to get rid of the decimals in the fahrenheit 
             var tempF = Math.round((response.list[0].main.temp - 273.15) * 1.80 + 32);
@@ -68,9 +67,9 @@ function getWeather(cityName) {
 
                 console.log(fiveDay[i]);
                 $("#fiveDay" + (i + 1)).append('<p class="card-text fiveDate">' + moment(fiveDay[i].dt_txt).format("MMM Do, YYYY") + '</p>')
+                $("#fiveDay" + (i + 1)).append('<p class="card-text">' + ("<img src='http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>"), + '</p>')
                 $("#fiveDay" + (i + 1)).append('<p class="card-text">' + "Temperature: " + fiveDayTempF + ("°"), '</p>')
                 $("#fiveDay" + (i + 1)).append('<p class="card-text">' + "Humidity: " + fiveDay[i].main.humidity + (" %"), '</p>')
-                $("#fiveDay" + (i + 1)).append('<p class="card-text">' + ("<img src='http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>"), + '</p>')
 
             }
         });
@@ -92,7 +91,7 @@ if (currentCities) {
         const currentCity = currentCities[i]
 
         // created buttons for locally stored cities, creates a new button for each city up to 6
-        $("#storedCities").append(' <button id="' + "city" + i + '" class="btn btn-sm ml-3 col-5">' + currentCity + '</button>');
+        $("#storedCities").append(' <button id="' + "city" + i + '" class="city-button btn btn-sm ml-3 col-5">' + currentCity + '</button>');
         $("#city" + i).click(function () {
             getWeather(currentCity);
 
